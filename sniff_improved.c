cc724f8c3fcf73ba911658377ed38d9276d316f9
choi-yunseo-code <choiyunseoda@gmail.com>
2026-07-05 23:37:09 +0900

    Update sniff_improved.c

--- sniff_improved.c
+++ sniff_improved.c
@@ -3,82 +3,39 @@
 #include <pcap.h>
 #include <arpa/inet.h>
 
+#include "myheader.h"
 
-/* Ethernet header */
-struct ethheader {
-  u_char  ether_dhost[6]; /* destination host address */
-  u_char  ether_shost[6]; /* source host address */
-  u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
-};
-
-/* IP Header */
-struct ipheader {
-  unsigned char      iph_ihl:4, //IP header length
-                     iph_ver:4; //IP version
-  unsigned char      iph_tos; //Type of service
-  unsigned short int iph_len; //IP Packet length (data + header)
-  unsigned short int iph_ident; //Identification
-  unsigned short int iph_flag:3, //Fragmentation flags
-                     iph_offset:13; //Flags offset
-  unsigned char      iph_ttl; //Time to Live
-  unsigned char      iph_protocol; //Protocol type
-  unsigned short int iph_chksum; //IP datagram checksum
-  struct  in_addr    iph_sourceip; //Source IP address
-  struct  in_addr    iph_destip;   //Destination IP address
-};
-
-void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                              const u_char *packet)
+void print_mac(const u_char *mac)
 {
-  struct ethheader *eth = (struct ethheader *)packet;
-
-  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-    struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
-
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
-
-    /* determine protocol */
-    switch(ip->iph_protocol) {                                 
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
-    }
-  }
+    printf("%02x:%02x:%02x:%02x:%02x:%02x",
+           mac[0], mac[1], mac[2],
+           mac[3], mac[4], mac[5]);
 }
 
-int main()
+void got_packet(u_char *args,
+                const struct pcap_pkthdr *header,
+                const u_char *packet)
 {
-  pcap_t *handle;
-  char errbuf[PCAP_ERRBUF_SIZE];
-  struct bpf_program fp;
-  char filter_exp[] = "icmp";
-  bpf_u_int32 net;
+    if (header->caplen < sizeof(struct ethheader))
+        return;
 
-  // Step 1: Open live pcap session on NIC with name enp0s3
-  handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
+    struct ethheader *eth = (struct ethheader *)packet;
 
-  // Step 2: Compile filter_exp into BPF psuedo-code
-  pcap_compile(handle, &fp, filter_exp, 0, net);
-  if (pcap_setfilter(handle, &fp) !=0) {
-      pcap_perror(handle, "Error:");
-      exit(EXIT_FAILURE);
-  }
+    /* IPv4가 아니면 무시 */
+    if (ntohs(eth->ether_type) != 0x0800)
+        return;
 
-  // Step 3: Capture packets
-  pcap_loop(handle, -1, got_packet, NULL);
+    if (header->caplen <
+        sizeof(struct ethheader) + sizeof(struct ipheader))
+        return;
 
-  pcap_close(handle);   //Close the handle
-  return 0;
-}
+    struct ipheader *ip = (struct ipheader *)
+        (packet + sizeof(struct ethheader));
 
+    /* TCP가 아니면 무시 */
+    if (ip->iph_protocol != IPPROTO_TCP)
+        return;
+
+    int ip_header_len = ip->iph_ihl * 4;
+
+    if (ip_header_len 
